975038ca7fe5ec24f3d3e62a0dcd2ec3a9fa6498
GeorgiPachov <georgi.patchov@gmail.com>
2014-04-25 23:00:53 +0300

    Initial commit

---
+++ src/com/ngm/explaintome/FirstActivity.java
@@ -0,0 +1,24 @@
+package com.ngm.explaintome;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class FirstActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_first);
+
+	
+	
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.first, menu);
+		return true;
+	}
+
+}
